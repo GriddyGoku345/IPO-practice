d850be559bd02222448d6d42a9c8799a445facf6
GriddyGoku345 <bissonetteis924@northwested.org>
2024-09-19 09:12:58 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,20 @@
+#Isaac Bissonette
+#9/18/24
+
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+
+total = num1 + num2
+
+print(f'the sum of {num1} and {num2} is {total}')
+
+num3 = float(input('Enter a number to divide your total: '))
+total2 = total / num3
+
+print(total2)
+
+num4 = float(input('Enter a number to multiply: '))
+
+total3 = total2 * num4
+
+print(total3)
